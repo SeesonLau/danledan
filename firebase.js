44a1845a2341068e974caa6f50d5a44b5a78c7d7
deandoiii <dosonshi@gmail.com>
2025-02-09 13:11:54 +0800

    init-firebase
    firebase base initialization setup

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAU6pu8Kq4oOSRep6tEp_41ZiPz6uV3Lps",
+  authDomain: "opticare-firebase.firebaseapp.com",
+  projectId: "opticare-firebase",
+  storageBucket: "opticare-firebase.firebasestorage.app",
+  messagingSenderId: "7823200320",
+  appId: "1:7823200320:web:9ff0e25f3e22bf80d04fa5",
+  measurementId: "G-J1PEB6SE72",
+};
+
+// init firebase app
+initializeApp(firebaseConfig);
