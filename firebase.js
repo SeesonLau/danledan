1cc338ebedc10be826225e86bee16dbcad90adf0
deandoiii <dosonshi@gmail.com>
2025-02-13 19:05:40 +0800

    temp update

--- firebase.js
+++ firebase.js
@@ -1,4 +1,6 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -13,3 +15,51 @@ const firebaseConfig = {
 
 // init firebase app
 initializeApp(firebaseConfig);
+
+//init services
+const db = getFirestore();
+const auth = getAuth();
+
+//collection ref
+const colRef = collection(db, "books");
+
+//get collection data
+getDocs(colRef)
+  .then((snapshot) => {
+    let books = [];
+    snapshot.docs.forEach((doc) => {
+      books.push({ ...doc.data(), id: doc.id });
+    });
+    console.log(books);
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+//adding documents
+const addBookForm = document.querySelector(".add");
+addBookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+
+//signing users up
+//const signupForm = document.querySelector
+
+// logging in
+const loginForm = document.querySelector(".login");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = loginForm.email.value;
+  const password = loginForm.password.value;
+
+  signInWithEmailAndPassword(auth)
+    .then((cred) => {
+      console.log("user logged in: ", cred.user);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+export { db, auth };
