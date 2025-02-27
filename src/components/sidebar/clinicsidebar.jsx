a5fbbcc31cab0c3e6d0e9a04a0171cd4f36bf651
Daniel Montesclaros <147793265+constantine2003@users.noreply.github.com>
2025-02-27 20:16:55 +0800

    FIXED SIDEBAR
    ## Fix: Ensure Correct Routing
    
    ### Changes:
    -Sidebars routing is now accurate

--- src/components/sidebar/clinicsidebar.jsx
+++ src/components/sidebar/clinicsidebar.jsx
@@ -5,60 +5,63 @@ import dynamic from 'next/dynamic';  // Import dynamic from Next.js
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faHouse, faCalendar, faNotesMedical, faGear, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import styles from '@/styles/clinic-homepage/clinic-sidebar.module.css';
-import Link from "next/link";
+import Link from 'next/link';
 
-function ClinicSidebar() {
-  return (
-    <aside className={styles.clinicsidebar}>
-      <ul>
-        <div className={styles.cliniclogodiv1}>
-          <img src="/landing-page-iamge/opto2.png" alt="Clinic Logo" />
-        </div>
+function ClinicSidebar () {
 
-        <div className={styles.cliniclogodiv}>
-          <Link href="/clinic-homepage">
-            <div className={styles.clinicicondiv}>
-              <FontAwesomeIcon icon={faHouse} />
+    return (
+    <div>
+        <aside className={styles.clinicsidebar}>
+            <ul>
+          <div className={styles.cliniclogodiv1}>
+             <img src="/landing-page-iamge/opto2.png" alt="Clinic Logo" />
+          </div>
+
+          <Link href="/clinic-homepage" passHref>
+            <div className={styles.cliniclogodiv}>
+              <div className={styles.clinicicondiv}>
+                <FontAwesomeIcon icon={faHouse} />
+              </div>
             </div>
           </Link>
-        </div>
-
-        <div className={styles.cliniclogodiv}>
-          <Link href="/clinic/clinic-appointments">
-            <div className={styles.clinicicondiv}>
-              <FontAwesomeIcon icon={faCalendar} />
+          
+          <Link href="/clinic/clinic-appointments" passHref>
+            <div className={styles.cliniclogodiv}>
+              <div className={styles.clinicicondiv}>
+                <FontAwesomeIcon icon={faCalendar} />
+              </div>
             </div>
           </Link>
-        </div>
 
-        <div className={styles.cliniclogodiv}>
-          <Link href="/clinic/clinic-ehr">
-            <div className={styles.clinicicondiv}>
-              <FontAwesomeIcon icon={faNotesMedical} />
+          <Link href="/clinic/clinic-ehr" passHref>
+            <div className={styles.cliniclogodiv}>
+              <div className={styles.clinicicondiv}>
+                <FontAwesomeIcon icon={faNotesMedical}/>
+              </div>
             </div>
           </Link>
-        </div>
 
-        <div className={styles.cliniclogodiv}>
-          <Link href="/clinic/clinic-settings">
-            <div className={styles.clinicicondiv}>
-              <FontAwesomeIcon icon={faGear} />
-              <FontAwesomeIcon icon={faRightFromBracket}/>            
-
+          <Link href="/clinic/clinic-settings" passHref>
+            <div className={styles.cliniclogodiv}>
+              <div className={styles.clinicicondiv}>
+                <FontAwesomeIcon icon={faGear}/> 
+              </div>
             </div>
           </Link>
-        </div>
 
-        <div className={styles.cliniclogodiv}>
-          <Link href="/">
-            <div className={styles.clinicicondiv}>
-              <FontAwesomeIcon icon={faRightFromBracket} />
+          <Link href="/" passHref>
+            <div className={styles.cliniclogodiv}>
+              <div className={styles.clinicicondiv}>
+                <FontAwesomeIcon icon={faRightFromBracket}/>            
+              </div>
             </div>
           </Link>
-        </div>
-      </ul>
-    </aside>
+
+        </ul>
+      </aside>
+    </div>
   );
-}
+};
+
 
 export default ClinicSidebar;
