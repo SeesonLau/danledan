5530bbcf5bf46aca140612604d432b7e4fe9aa87
John Laurence Sison <seesonjohnlau@gmail.com>
2025-03-30 04:36:03 +0800

    5th Update clinic-homepage

--- src/components/clinic-layout.jsx
+++ src/components/clinic-layout.jsx
@@ -1,73 +1,12 @@
-import React from "react";
-import styles from "../styles/clinic-ehr/clinic-ehr-textbox.module.css";
+import ClinicSidebar from './sidebar/clinicsidebar';
 
-export const EHRTextbox = ({ label, value, onChange }) => {
+export default function ClinicLayout({ children }) {
   return (
-    <div className={styles.ehrTextbox}>
-      <label className={styles.ehrTextboxLabel}>{label}</label>
-      <input
-        type="text"
-        value={value}
-        onChange={onChange}
-        className={styles.ehrTextboxContent}
-      />
+    <div style={{ display: 'flex' }}>
+        <ClinicSidebar />
+        <main style={{ flex: 1 }}>
+            {children}
+        </main>
     </div>
-  );
-};
-
-export const EHR2Textbox = ({ label, value, onChange }) => {
-  return (
-    <div className={styles.ehrTextbox2}>
-      <label className={styles.ehrTextboxLabel2}>{label}</label>
-      <input
-        type="text"
-        value={value}
-        onChange={onChange}
-        className={styles.ehrTextboxContent}
-      />
-    </div>
-  );
-};
-
-export const EHR3Textbox = ({ label, value, onChange }) => {
-  return (
-    <div className={styles.ehrTextbox3}>
-      <label className={styles.ehrTextboxLabel3}>{label}</label>
-      <input
-        type="text"
-        value={value}
-        onChange={onChange}
-        className={styles.ehrTextboxContent}
-      />
-    </div>
-  );
-};
-
-export const EHR4Textbox = ({ label, value, onChange }) => {
-  return (
-    <div className={styles.ehrTextbox4}>
-      <label className={styles.ehrTextboxLabel4}>{label}</label>
-      <input
-        type="text"
-        value={value}
-        onChange={onChange}
-        className={styles.ehrTextboxContent4}
-      />
-    </div>
-  );
-};
-
-export const EHR5Textbox = ({ label, value, onChange }) => {
-  return (
-    <div className={styles.ehrTextbox5}>
-      <label className={styles.ehrTextboxLabel5}>{label}</label>
-      <textarea
-        className={styles.ehrTextarea5}
-        value={value}
-        onChange={onChange}
-        rows="4"
-        cols="50"
-      />
-    </div>
-  );
-};
+);
+}
